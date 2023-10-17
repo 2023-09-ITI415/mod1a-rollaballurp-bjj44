12970a03d3c339003390a596442e233e807a86c9
bjj44 <144397218+bjj44@users.noreply.github.com>
2023-10-17 04:39:09 -0400

    Final Edit

--- Assets/Scripts/PlayerConrtoller.cs
+++ Assets/Scripts/PlayerConrtoller.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerConrtoller : MonoBehaviour
 {
     public float speed = 0;
 
+    public TextMeshProUGUI countText;
+
+    public GameObject winTextObject;
+
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
 
@@ -15,6 +21,12 @@ public class PlayerConrtoller : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        count = 0;
+
+        SetCountText();
+
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -26,10 +38,32 @@ public class PlayerConrtoller : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if(count >= 12) {
+            winTextObject.SetActive(true);
+        }
+    }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp")) 
+        {
+             other.gameObject.SetActive(false);
+
+             count = count + 1;
+
+             SetCountText();
+        }
+       
     }
 }
